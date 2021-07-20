dcec2fb39ba2c1dc0a72a6b155830d1f5acddd92
Jean-Marc Epis <jeanmarc.epis@gmail.com>
2021-07-20 14:10:55 +0200

    Fixed logo location

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Logo from './public/assets/logo.png';
 
 function App() {
   return (
     <div className="App">
+      <img alt="logo" src={Logo} />
       <h1>Hello Project</h1>
     </div>
   );
